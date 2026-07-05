d55333a1163d25557dfb06d942a26a5e1fda5265
Lakshay Dhanda <lakshaydh72@gmail.com>
2026-07-05 09:22:44 +0530

    Add files via upload

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 12345;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "123456";
+accountCity = "Delhi";
+let accountState;
+
+//accountId = 8
+
+accountEmail = "hitesh@yopmail.com";
+accountPassword = "12345678";
+accountCity = "Pune";
+
+
+console.log(accountId);
+
+/*
+Prefer not use var
+Because of issue in block scope and function scope
+*/
+
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version 
+
+//alert(3+3);  // we are using node.js, not browser
+
+
+console.log(3+3);
+console.log("Hitesh");//code readability should be high.
+
+let name = "Hitesh";
+let age = 21;
+let isLoggedIn = false;
+
+// number => 2 to the power of 53;
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object 
+
+console.log(typeof "lakshay");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
